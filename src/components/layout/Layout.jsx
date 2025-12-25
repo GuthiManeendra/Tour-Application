26153d98170c5225ae92f63bacfcc662142bb52c
GuthiManeendra <maneendraguthi@gmail.com>
2025-12-25 17:09:59 +0530

    header component with navigation UI

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,10 @@
+import Header from "../header/Header"
 import { Routes } from "../routing/Routes"
 
 export const Layout =()=>{
     return(
         <>
+        <Header/>
         <Routes/>
         </>
     )
